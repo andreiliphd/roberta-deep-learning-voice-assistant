b3f23f8c2c76a673d41926383180e65411d58f59
Andrei Li <andreiliphd@gmail.com>
2020-11-05 09:01:44 +0300

    Added Ruberta model from HuggingFace.

--- application.py
+++ application.py
@@ -1,29 +1,51 @@
 import json
 import mmh3
-import torch
 import flask
 from flask import Flask, request
+import torch
+from transformers import pipeline
+from transformers import AutoTokenizer, AutoModel
 
-sessionStorage = {}
+
+tokenizer = AutoTokenizer.from_pretrained("DeepPavlov/rubert-base-cased-conversational")
+model = AutoModel.from_pretrained("DeepPavlov/rubert-base-cased-conversational", return_dict=True)
 
 app = Flask(__name__)
 
-class Net(torch.nn.Module):
+for par in model.parameters():
+    par.requires_grad = False
+
+class RobertaClassifier(torch.nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = torch.nn.Linear(1, 256)
-        self.fc2 = torch.nn.Linear(256, 256)
-        self.fc3 = torch.nn.Linear(256, 128)
-        self.fc4 = torch.nn.Linear(128, 2)
+        super(RobertaClassifier, self).__init__()
+        self.fc1 = torch.nn.Linear(393216, 128)
+        self.fc2 = torch.nn.Linear(128, 128)
+        self.fc3 = torch.nn.Linear(128, 2)
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x):
+        x = model(**x)
+        x = x.last_hidden_state.view(x.last_hidden_state.size()[0], -1)
         x = torch.nn.functional.relu(self.fc1(x))
         x = torch.nn.functional.relu(self.fc2(x))
-        x = torch.nn.functional.relu(self.fc3(x))
-        return self.fc4(x)
+        x = self.fc3(x)
+        return x
 
 
-model = Net()
+classifier = RobertaClassifier()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(classifier.parameters(), lr=0.0005)
+
+
+def train(input, label):
+    model.train()
+    output = classifier(input)
+    loss = criterion(output, label)
+    optimizer.zero_grad()
+    loss.backward()
+    print(loss.item())
+    optimizer.step()
+    return output
 
 
 @app.route("/", methods=['POST'])
@@ -57,11 +79,8 @@ def main():
         )
 
     request_json = request.json
-    hash = mmh3.hash(request_json['request']['original_utterance'])
-    norm = hash / 4294967295
-    norm = torch.tensor(norm)
-    output = norm.reshape([1, 1])
-    output = model(output)
+    input = tokenizer(request_json['request']['original_utterance'], return_tensors="pt", padding='max_length', max_length=512)
+    output = train(input, torch.tensor(1).reshape(1))
     argmax = torch.argmax(output, dim=1)
     response['response']['text'] = "Категория классификации " + str(int(argmax))
     return json.dumps(
